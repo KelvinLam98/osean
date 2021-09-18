e38e079f59bfbd9c12913e92531ae04bd2f20d8d
Kelvin <hylam98tom@gmail.com>
2021-09-18 12:12:45 +0800

    Successfully build database table and add data to table

--- app/models/PersonalData.scala
+++ app/models/PersonalData.scala
@@ -1,15 +1,20 @@
 package models
 
+import anorm.SqlParser.scalar
+
 import javax.xml.bind.annotation.XmlType.DEFAULT
 import anorm._
+
 import java.sql.Connection
-import play.api.libs.json.{Writes, Json}
+import play.api.libs.json.{Json, Writes}
 
 case class PersonalData(id: Option[Long], name: String, age: Int, address: String)
 
 object PersonalData {
 
   implicit val writes: Writes[PersonalData] = Json.writes[PersonalData]
+
+  val parser: RowParser[PersonalData] = Macro.namedParser[PersonalData]
 
   val DEFAULT_ID: Long = 1
 
@@ -22,6 +27,10 @@ object PersonalData {
     )
   }
 
+  def findAll(implicit conn: Connection): Seq[PersonalData] = {
+    SQL("select * from PersonalData").on().as(parser.*)
+  }
+
   def insert(personalData: PersonalData)(implicit conn: Connection): Long = {
     SQL("insert into PersonalData (name, age, address) values ({name}, {age}, {address})").on(
       "name" -> personalData.name,
@@ -29,13 +38,41 @@ object PersonalData {
       "address" -> personalData.address
     ).executeInsert().get
   }
-//
-//  def findById(id: Long)(implicit conn: Connection): Option[PersonalData] = {
-//    SQL("select * from Currencies where id = {id}").on(
-//      "id" -> id
-//    ).singleOpt().map(toModel)
-//  }
 
+  def findById(id: Long)(implicit conn: Connection): Option[PersonalData] = {
+    SQL("select * from PersonalData where id = {id}").on(
+      "id" -> id
+    ).as(parser.singleOpt)
+  }
+
+  def countAll(implicit conn: Connection): Long = {
+    SQL("select count(*) as count from PersonalData").as(SqlParser.long("count").single)
+
+  }
+
+  def countFiltered(searchText: String)(implicit conn: Connection): Long = {
+    val searchCriteria =
+      if(searchText.isEmpty)
+        ""
+      else
+        "where (code like {searchText} or description like {searchText})"
+      SQL("select count(*) as count from PersonalData " + searchCriteria).on(
+        "searchText" -> ("%" + searchText + "%")
+      ).as(SqlParser.long("count").single)
+  }
+
+  def search(start: Long, count: Long, searchText: String)(implicit conn: Connection): Seq[PersonalData] = {
+    val searchCriteria =
+      if(searchText.isEmpty)
+        ""
+      else
+        "where (code like {searchText} or description like {searchText})"
+    SQL("select * from Currencies " + searchCriteria + " order by code Asc limit {start}, {count}").on(
+      "start" -> start,
+      "count" -> count,
+      "searchText" -> ("%" + searchText + "%")
+    ).as(parser.*)
+  }
 
 }
 
